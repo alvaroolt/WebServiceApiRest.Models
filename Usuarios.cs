b8285c7b82c971ada86d3c388c1534a1fcb21a15
Alvaro L <alvarooleto@gmail.com>
2022-05-20 13:45:41 +0200

    autenticacion

--- Usuarios.cs
+++ Usuarios.cs
@@ -13,15 +13,22 @@ namespace WebServiceApiRest.Models
         public int user_id { get; set; }
         public string user_nom { get; set; }
         public string user_pwd { get; set; }
-        public int user_admin { get; set; }
+        public RolUsuario user_rol { get; set; } = RolUsuario.Usuario;
 
         public Usuarios() { }
 
-        public Usuarios(int user_id, string user_nom, string user_pwd)
+        public Usuarios(int user_id, string user_nom, string user_pwd, RolUsuario user_rol)
         {
             this.user_id = user_id;
             this.user_nom = user_nom;
             this.user_pwd = user_pwd;
+            this.user_rol = user_rol;
+        }
+
+        public enum RolUsuario : int
+        {
+            Administrador = 0,
+            Usuario = 1
         }
     }
 }
